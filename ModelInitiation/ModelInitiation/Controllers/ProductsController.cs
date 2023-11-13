1be0e1ab15a23f6be2062fe0eaa0f66d23f3af16
Tibo Pfeifer <tibo.pfeifer@cgi.com>
2023-11-13 10:13:12 +0100

    Section 07: Model Initiation

---
+++ ModelInitiation/ModelInitiation/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using ModelInitiation.Models;
+
+namespace ModelInitiation.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product() { ProductId = 101, ProductName = "AC", Rate = 45000 },
+                new Product() { ProductId = 102, ProductName = "Mobile", Rate = 38000 },
+                new Product() { ProductId = 103, ProductName = "Bike", Rate = 94000 }
+            };
+            ViewBag.products = products;
+            return View(products);
+        }
+        
+        public ActionResult Details(int id)
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product() { ProductId = 101, ProductName = "AC", Rate = 45000 },
+                new Product() { ProductId = 102, ProductName = "Mobile", Rate = 38000 },
+                new Product() { ProductId = 103, ProductName = "Bike", Rate = 94000 }
+            };
+            Product matchingProduct = null;
+            foreach (var item in products)
+            {
+                if (item.ProductId == id)
+                {
+                    matchingProduct = item;
+                }
+            }
+            ViewBag.MatchingProduct = matchingProduct;
+            return View(matchingProduct);
+        }
+    }
+    
+}
---
+++ ModelInitiation/ModelInitiation/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace ModelInitiation.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public double Rate { get; set; }
+    }
+}
