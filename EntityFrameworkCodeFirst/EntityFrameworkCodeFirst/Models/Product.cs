108b0d05a9f992fb3633b24cdaf40fa7a72cdd44
Tibo Pfeifer <tibo.pfeifer@cgi.com>
2023-11-14 15:52:14 +0100

    Section 11: Html Helpers Initiation

--- EntityFrameworkCodeFirst/EntityFrameworkCodeFirst/Models/Product.cs
+++ EntityFrameworkCodeFirst/EntityFrameworkCodeFirst/Models/Product.cs
@@ -8,18 +8,35 @@ namespace EntityFrameworkCodeFirst.Models
     public class Product
     {
         [Key]
+        [Display(Name = "Product Id")]
         public long ProductID { get; set; }
+        
+        [Display(Name = "Product Name")]
         public string ProductName { get; set; }
+        
+        [Display(Name = "Price")]
         public decimal? Price { get; set; }
         
         [Column("DateOfPurchase", TypeName = "datetime")]
-        public System.DateTime? Dop { get; set; }
+        [Display(Name = "Date of Purchase")]
+        public DateTime? Dop { get; set; }
+        
+        [Display(Name = "Availability Status")]
         public string AvailabilityStatus { get; set; }
+        
+        [Display(Name = "Category Id")]
         public long? CategoryID { get; set; }
+        
+        [Display(Name = "Brand Id")]
         public long? BrandID { get; set; }
+        
+        [Display(Name = "Active")]
         public bool? Active { get; set; }
+        
+        [Display(Name = "Photo")]
         public string Photo { get; set; }
 
+        [Display(Name = "Quantity")]
         public decimal? Quantity { get; set; }
 
         public virtual Brand Brand { get; set; }
---
+++ EntityFrameworkCodeFirst/EntityFrameworkCodeFirst/Views/Helpers/FileHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace EntityFrameworkCodeFirst.Views.Helpers
+{
+    public static class FileHelper
+    {
+        public static MvcHtmlString File(this HtmlHelper htmlHelper, string cssClassName)
+        {
+            TagBuilder tag = new TagBuilder("input");
+            tag.MergeAttribute("type", "file");
+            tag.MergeAttribute("id", "Image");
+            tag.MergeAttribute("name", "Photo");
+            tag.MergeAttribute("class", cssClassName);
+            return MvcHtmlString.Create(tag.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
