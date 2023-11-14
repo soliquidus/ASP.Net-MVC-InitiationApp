7de09456b00764a3b1f36404d6184d01d1b6285a
Tibo Pfeifer <tibo.pfeifer@cgi.com>
2023-11-14 11:46:52 +0100

    Section 10: EF Code First Basics

---
+++ EntityFrameworkCodeFirst/EntityFrameworkCodeFirst/Controllers/CategoriesController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using EntityFrameworkCodeFirst.Models;
+
+namespace EntityFrameworkCodeFirst.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET
+        public ActionResult Index()
+        {
+            CompanyDbContext db = new CompanyDbContext();
+            List<Category> categories = db.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
---
+++ EntityFrameworkCodeFirst/EntityFrameworkCodeFirst/Models/Brand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityFrameworkCodeFirst.Models
+{
+    public class Brand
+    {
+        [Key]
+        public long BrandID { get; set; }
+        public string BrandName { get; set; }
+    }
+}
+
+
