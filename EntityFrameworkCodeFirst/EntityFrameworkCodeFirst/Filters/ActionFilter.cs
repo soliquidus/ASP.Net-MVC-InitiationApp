22c14f73968b9c6fb4895dee4c5928a6be143c8e
Tibo Pfeifer <tibo.pfeifer@cgi.com>
2023-11-15 16:05:24 +0100

    Section 14: Action & Result Filters

---
+++ EntityFrameworkCodeFirst/EntityFrameworkCodeFirst/Filters/ActionFilter.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace EntityFrameworkCodeFirst.Filters
+{
+    public class ActionFilter : FilterAttribute, IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            filterContext.Controller.ViewBag.NoOfVisitorsOfTheDay = 50;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            filterContext.Controller.ViewBag.NoOfVisitorsOfTheDay = 70;
+        }
+    }
+}
