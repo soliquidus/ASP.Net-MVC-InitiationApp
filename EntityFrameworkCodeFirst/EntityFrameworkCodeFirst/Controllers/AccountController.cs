a39bd7e959a732d9ac2495a08f641c62159545d3
Tibo Pfeifer <tibo.pfeifer@cgi.com>
2023-11-15 11:09:42 +0100

    Section 13: Identity registration & login/logout

---
+++ EntityFrameworkCodeFirst/EntityFrameworkCodeFirst/Controllers/AccountController.cs
@@ -0,0 +1,96 @@
+﻿using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using EntityFrameworkCodeFirst.Identity;
+using EntityFrameworkCodeFirst.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+
+namespace EntityFrameworkCodeFirst.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: Account/Register
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel registerViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // Registration
+                var appDbContext = new ApplicationDbContext();
+                var userStore = new ApplicationUserStore(appDbContext);
+                var userManager = new ApplicationUserManager(userStore);
+                var passwordHash = Crypto.HashPassword(registerViewModel.Password);
+                var user = new ApplicationUser()
+                {
+                    Email = registerViewModel.Email,
+                    UserName = registerViewModel.Username,
+                    PasswordHash = passwordHash,
+                    City = registerViewModel.City,
+                    Country = registerViewModel.Country,
+                    Birthday = registerViewModel.DateOfBirth,
+                    Address = registerViewModel.Address,
+                    PhoneNumber = registerViewModel.Mobile
+                };
+                IdentityResult result = userManager.Create(user);
+                
+                if (result.Succeeded)
+                {
+                    // Role
+                    userManager.AddToRole(user.Id, "Customer");
+
+                    // Login
+                    var authenticationManager = HttpContext.GetOwinContext().Authentication;
+                    var userIdentity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+                    authenticationManager.SignIn(new AuthenticationProperties(), userIdentity);
+                }
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("Registration error", "Invalid data");
+            return View();
+        }
+
+        // GET: Account/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Account/Login
+        [HttpPost]
+        public ActionResult Login(LoginViewModel loginViewModel)
+        {
+            // Login
+            var appDbContext = new ApplicationDbContext();
+            var userStore = new ApplicationUserStore(appDbContext);
+            var userManager = new ApplicationUserManager(userStore);
+            var user = userManager.Find(loginViewModel.Username, loginViewModel.Password);
+            if (user != null)
+            {
+                // Login
+                var authenticationManager = HttpContext.GetOwinContext().Authentication;
+                var userIdentity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+                authenticationManager.SignIn(new AuthenticationProperties(), userIdentity);
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("Login error", "Invalid username or password");
+            return View();
+        }
+        
+        // GET: Account/Logout
+        public ActionResult Logout()
+        {
+            var authenticationManager = HttpContext.GetOwinContext().Authentication;
+            authenticationManager.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ EntityFrameworkCodeFirst/EntityFrameworkCodeFirst/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EntityFrameworkCodeFirst.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Username can't be blank")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password can't be blank")]
+        public string Password { get; set; }
+    }
+}
