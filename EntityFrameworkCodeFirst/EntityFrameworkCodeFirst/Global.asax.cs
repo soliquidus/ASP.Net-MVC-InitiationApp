73fbda5df905e6ae190015a2e7f0494e2917013e
Tibo Pfeifer <tibo.pfeifer@cgi.com>
2023-11-17 16:48:52 +0100

    Section 17: Exception Handling Initiation

--- EntityFrameworkCodeFirst/EntityFrameworkCodeFirst/Global.asax.cs
+++ EntityFrameworkCodeFirst/EntityFrameworkCodeFirst/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System.Web;
+﻿using System;
+using System.IO;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -11,6 +13,16 @@ namespace EntityFrameworkCodeFirst
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+        }
+
+        protected void Application_Error()
+        {
+            Exception exec = Server.GetLastError();
+            string s = "Message: " + exec.Message + ", Type: " + exec.GetType().ToString() + ", Source: " + exec.Source;
+            StreamWriter sw = File.AppendText(HttpContext.Current.Request.PhysicalApplicationPath + "\\ErrorLog.txt");
+            sw.WriteLine(s);
+            sw.Close();
+            //Response.Redirect("~/Error.html");
         }
     }
 }
